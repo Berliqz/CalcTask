79c2e38cddced9637d40b94615323bc1b0b42727
Дмитрий Дмитрий <dmitry-okrujko@ya.ru>
2021-10-26 17:40:21 +0400

    Добавьте файлы проекта.

---
+++ CalcTask/Figure.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CalcTask
+{
+    public abstract class Figure
+    {
+        public abstract string FigureName();
+        public abstract string Area();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(
+                $"Название фигуры: {FigureName()}\n" +
+                $"Площадь: {Area()}\n"
+                );
+            Console.WriteLine();
+        }
+        public class Triangle : Figure
+        {
+            double sideA, sideB, sideC;   // стороны 
+            public Triangle(double triangleSideA, double triangleSideB, double triangleSideC)
+            {
+                sideA = triangleSideA;
+                sideB = triangleSideB;
+                sideC = triangleSideC;
+            }
+            // вычисление площади 
+            public override string Area()
+            {
+                double semPer = (sideA + sideB + sideC) / 2;
+                return Math.Sqrt(semPer * (semPer - sideA) * (semPer - sideB) * (semPer - sideC)).ToString();
+            }
+            //возврат названия
+            public override string FigureName()
+            {
+                return "Треугольник";
+            }
+        }
+        public class Circle : Figure
+        {
+            int radius;
+            public Circle(int rad)
+            {
+                radius = rad;
+            }
+            public override string Area()
+            {
+                double Circuit = 2.0 * Math.PI * radius;
+                return Circuit.ToString();
+            }
+            public override string FigureName()
+            {
+                return "Круг";
+            }
+        }
+    }
+}
